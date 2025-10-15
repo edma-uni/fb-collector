159ceaf3fb4d62a0d3938f7e8bd02811324b1695
takeshiki <dimav2206@gmail.com>
2025-10-16 00:18:27 +0300

    added testing and pine

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,50 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MetricsModule } from './metrics/metrics.module';
 import { EventModule } from './event/event.module';
+import { randomUUID } from 'crypto';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  singleLine: true,
+                  translateTime: 'yyyy-mm-dd HH:MM:ss',
+                  ignore: 'pid,hostname',
+                },
+              }
+            : undefined,
+        formatters: {
+          level: (label: string) => {
+            return { level: label };
+          },
+        },
+        customProps: () => ({
+          correlationId: randomUUID(),
+        }),
+        serializers: {
+          req: (req: any) => ({
+            method: req.method,
+            url: req.url,
+          }),
+          res: (res: any) => ({
+            statusCode: res.statusCode,
+          }),
+        },
+      },
+    }),
     PrometheusModule.register({
       defaultMetrics: {
         enabled: true,
@@ -28,6 +63,6 @@ import { EventModule } from './event/event.module';
     EventModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
